9b1b89d7fa30bd5194df0e58c421ceefe597b71c
Chris Regenboog <chrisregenboog@protonmail.com>
2018-04-25 13:40:54 +0200

    added named scratchpads

--- xmonad.hs
+++ xmonad.hs
@@ -1,20 +1,12 @@
 import XMonad
+import qualified XMonad.StackSet as W
 import XMonad.Util.EZConfig (additionalKeysP)
 import XMonad.Layout.Spacing
 -- import XMonad.Hooks.WallpaperSetter
 -- import Xmonad.Util.SpawnOnce
 import XMonad.Prompt as P
 import XMonad.Prompt.Man
-
-main = xmonad $ def
-  { terminal    = myTerminal
-  , modMask     = myModMask
-  , borderWidth = 0
-  , layoutHook = smartSpacing 2 $ myLayoutHook
-  , workspaces = myWorkspaces
-  -- , logHook = myLogHook
-  -- , startupHook = myStartupHook
-  } `additionalKeysP` myKeys
+import XMonad.Util.NamedScratchpad
 
 myLauncher = "rofi -show run -theme arthur"
 myTerminal = "termite"
@@ -29,6 +21,27 @@ myWorkspaces = map show [1..9]
 myKeys = [
   ("<F1>", spawn myTerminal),
   ("<F5>", spawn myLauncher),
-  ("M-m", manPrompt P.def),
+  ("M-m", namedScratchpadAction scratchpads "monitor"),
+  ("M-t", namedScratchpadAction scratchpads "term"),
+  ("M-r", namedScratchpadAction scratchpads "ranger"),
+  -- ("M-m", manPrompt P.def),
   ("M-S-s", spawn "systemctl suspend")
   ]
+
+scratchpads = [ NS "monitor" (myTerminal ++ " -e htop") (title =? "htop") scratchFloat
+              , NS "term" (myTerminal ++ " --title term") (title =? "term") scratchFloat
+              , NS "ranger" (myTerminal ++ " -e ranger") (title =? "ranger") scratchFloat
+              ]
+  where scratchFloat = customFloating size
+        size = W.RationalRect (1/6) (1/6) (2/3) (2/3)
+
+main = xmonad $ def
+  { terminal    = myTerminal
+  , modMask     = myModMask
+  , borderWidth = 0
+  , layoutHook = smartSpacing 2 $ myLayoutHook
+  , workspaces = myWorkspaces
+  -- , logHook = myLogHook
+  -- , startupHook = myStartupHook
+  , manageHook = namedScratchpadManageHook scratchpads
+  } `additionalKeysP` myKeys
