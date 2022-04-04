f800e85afd5f91d0abe0e2d3c45e35a4cfef7515
Chris Regenboog <chris.regenboog@connectplaza.com>
2022-04-04 14:44:34 +0200

    added init.el

--- home.nix
+++ home.nix
@@ -88,32 +88,5 @@
   home.sessionVariables = {
     EDITOR = "nvim";
   };
-  home.file.".emacs.d/init.el".text = ''
-    (require 'package)
-    (setq package-archives nil)
-    (package-initialize)
-    (require 'use-package)
-
-    (setq standard-indent 2)
-    (scroll-bar-mode -1)
-    (tool-bar-mode -1)
-    (tooltip-mode -1)
-
-    (use-package evil
-      :config
-      (evil-mode)
-    )
-
-    (use-package kaolin-themes
-      :config
-      (load-theme 'kaolin-aurora t)
-    )
-
-    (use-package all-the-icons)
-
-    (use-package nix-mode
-      :mode "\\.nix\\'")
-
-    (use-package org)
-  '';
+  home.file.".emacs.d/init.el".source = ./init.el;
 }
